d024127ece4296d9486288fe5370e8c8c9283c5b
Lorena Góes <goesloren@gmail.com>
2021-04-27 23:15:05 -0300

    main files

---
+++ src/challenges.js
@@ -0,0 +1,86 @@
+// Desafio 1
+function compareTrue(a, b) {
+  // seu código aqui
+}
+
+// Desafio 2
+function calcArea(base, height) {
+  // seu código aqui
+}
+
+// Desafio 3
+function splitSentence(str) {
+  // seu código aqui
+}
+
+// Desafio 4
+function concatName(arrayStr) {
+  // seu código aqui
+}
+
+// Desafio 5
+function footballPoints(wins, ties) {
+  // seu código aqui
+}
+
+// Desafio 6
+function highestCount(numbers) {
+  // seu código aqui
+}
+
+// Desafio 7
+function catAndMouse(mouse, cat1, cat2) {
+  // seu código aqui
+
+}
+
+// Desafio 8
+function fizzBuzz(numbersArr) {
+  // seu código aqui
+}
+
+// Desafio 9
+function encode(myStr) {
+  // seu código aqui
+}
+
+function decode(myOtherStr) {
+  // seu código aqui
+}
+
+// Desafio 10
+function techList(tech, name) {
+  // seu código aqui
+}
+
+// Desafio 11
+function generatePhoneNumber(phoneNumber) {
+  // seu código aqui
+}
+
+// Desafio 12
+function triangleCheck(lineA, lineB, lineC) {
+  // seu código aqui
+}
+
+// Desafio 13
+function hydrate(drink) {
+  // seu código aqui
+}
+
+module.exports = {
+  calcArea,
+  catAndMouse,
+  compareTrue,
+  concatName,
+  decode,
+  encode,
+  fizzBuzz,
+  footballPoints,
+  generatePhoneNumber,
+  techList,
+  highestCount,
+  hydrate,
+  splitSentence,
+  triangleCheck,
+}
